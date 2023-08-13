5b6a98f3be216ec4406466ad1a4f80a935bd5a82
Kyeorae Yeom <duarufp06@gmail.com>
2023-08-13 22:44:03 +0900

    basic redux

--- src/slices/user.ts
+++ src/slices/user.ts
@@ -1,5 +1,13 @@
 import {createSlice} from '@reduxjs/toolkit';
 
+// store -> root reducer(state) -> user slice, order slice
+// ... state.user.email
+// ... state.order
+
+// action: state를 바꾸는 동작/행위
+// dispatch: 그 액션을 실제로 실행하는 함수
+// reducer: 액션이 실제로 실행되면 state를 바꾸는 로직
+
 const initialState = {
   name: '',
   email: '',
@@ -9,13 +17,14 @@ const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
+    // 동기 액션
     setUser(state, action) {
       state.email = action.payload.email;
       state.name = action.payload.name;
       state.accessToken = action.payload.accessToken;
     },
   },
-  extraReducers: builder => {},
+  extraReducers: builder => {}, // 비동기 액션
 });
 
 export default userSlice;
