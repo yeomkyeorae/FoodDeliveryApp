e4f18fe43f6ce6d0b919b467731c991138c9f0a9
DESKTOP-Q17AD2N\duarufp06 <duarufp06@coretrust.com>
2023-08-16 15:47:55 +0900

    make btns for accept, reject order

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -1,31 +1,15 @@
 import React, {useCallback} from 'react';
-import {
-  FlatList,
-  Pressable,
-  ScrollView,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import {FlatList} from 'react-native';
 import {useSelector} from 'react-redux';
 import {RootState} from '../store/reducer';
 import {Order} from '../slices/order';
+import EachOrder from '../components/EachOrder';
 
 function Orders() {
   const orders = useSelector((state: RootState) => state.order.orders);
 
-  const toggleDetail = useCallback(() => {}, []);
-
   const renderItem = useCallback(({item}: {item: Order}) => {
-    return (
-      <View style={styles.orderContainer}>
-        <Pressable onPress={toggleDetail} style={styles.info}>
-          <Text style={styles.eachInfo}>
-            {item.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
-          </Text>
-        </Pressable>
-      </View>
-    );
+    return <EachOrder item={item} />;
   }, []);
 
   return (
@@ -37,20 +21,5 @@ function Orders() {
     />
   );
 }
-
-const styles = StyleSheet.create({
-  orderContainer: {
-    borderRadius: 5,
-    margin: 5,
-    padding: 10,
-    backgroundColor: 'lightgray',
-  },
-  info: {
-    flexDirection: 'row',
-  },
-  eachInfo: {
-    flex: 1,
-  },
-});
 
 export default Orders;
