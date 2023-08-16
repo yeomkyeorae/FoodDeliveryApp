5309a86fd2e445e464a2f6444cef74c8d965b539
DESKTOP-Q17AD2N\duarufp06 <duarufp06@coretrust.com>
2023-08-16 14:55:13 +0900

    orderList by FlatList

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 
 import userSlice from '../slices/user';
+import orderSlice from '../slices/order';
 
 // 전체 상태
 const rootReducer = combineReducers({
   user: userSlice.reducer,
+  order: orderSlice.reducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
